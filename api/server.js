ec4267775a32fbc949899f8f2e5983a6d40f6900
SS-Jane <superjane.prodev@gmail.com>
2024-11-22 18:09:12 +0700

    create token from user signin

--- api/server.js
+++ api/server.js
@@ -1,8 +1,14 @@
 const express = require("express");
 const app = express();
+const bpdyParser = require('body-parser')
+const cors = require('cors')
 
 const userController = require("./controller/UserController");
+const bodyParser = require("body-parser");
 
+app.use(bodyParser.json())
+// app.use(bodyParser.urlencoded({ extended : true }))
+app.use(cors())
 
 app.use("/user", userController);
 
